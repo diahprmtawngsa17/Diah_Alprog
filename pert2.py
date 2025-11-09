34e3f642ea52636c460de3608ff29540d60d3eb7
diahprmtawngsa17 <diahprmtawngsa@gmail.com>
2025-11-09 20:46:22 +0800

    Add files via upload

---
+++ pert2.py
@@ -0,0 +1,32 @@
+# belajar variabel
+# cara kita menyimpan nilai
+
+# memberikan nilai (assigment)
+x = 10
+y = 6
+lebar = 13
+
+print(x + y + lebar)
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lebar =", lebar)
+
+# tings to consider when making a variabel
+# hal2 yang harus di perhatikan ketika membuat variabel
+
+bett99 = 9 # tidak boleh diawali dengan angka
+jumlah_hutang = 1000000 # jika menggunakan dua suku kata harus digabungkan dengan _
+JumlahHutang = 1000000 # ika menggunakan dua suku kata harus digabungkan dengan huruf kapital
+
+# pemanggilan indirect
+z = lebar
+print("nilai dari z =", z)
+
+#TUGAS
+#1. nama saya adalah : (nama)
+nama = "Diah Permata Wangsa"
+print("nama saya adalah" + nama)
+
+#2. nim saya adalah : (nim)
+nim = "25241110"
+print("nama saya adalah" + nim)
